58d7fce8469a669094c6e0541b84feb88689ce1e
andrejs <andrejs@localhost.localdomain>
2013-05-14 21:53:21 +0200

    master fixed login

--- config/autoload/db.global.php
+++ config/autoload/db.global.php
@@ -1,27 +1,18 @@
 <?php
 
-$dbParams = array(
-    'database'  => 'moneyzaurus',
-    'username'  => 'root',
-    'password'  => 'root',
-    'hostname'  => 'localhost',
-    'port'      => '3306',
-);
-
 return array(
     'db' => array(
         'driver'         => 'Pdo',
-        'dsn'            => 'mysql:dbname='.$dbParams['database'].';host='.$dbParams['hostname'],
-//        'username'  => $dbParams['username'],
-//        'password'  => $dbParams['password'],
+        'dsn'            => 'mysql:dbname=moneyzaurus;host=localhost',
+        'username'       => 'root',
+        'password'       => 'root',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-        ),
+        )
     ),
     'service_manager' => array(
         'factories' => array(
-            'Zend\Db\Adapter\Adapter'
-                    => 'Zend\Db\Adapter\AdapterServiceFactory',
-        ),
-    ),
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
+        )
+    )
 );
