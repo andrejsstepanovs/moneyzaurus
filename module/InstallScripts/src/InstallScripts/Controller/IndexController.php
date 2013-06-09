5e5966f2dc5208c2698412c7a0d8c3e198334799
Andrejs Stepanovs <andrejsstepanovs@gmail.com>
2013-06-09 16:31:25 +0200

    installsctipt implemented set command

--- module/InstallScripts/src/InstallScripts/Controller/IndexController.php
+++ module/InstallScripts/src/InstallScripts/Controller/IndexController.php
@@ -12,9 +12,12 @@ class IndexController extends AbstractActionController
 
     public function listAction()
     {
+        $storage = $this->getInstallScriptStorage()->getAdapter();
         $bundles = $this->getInstallScriptLocator()->getBundles();
         foreach ($bundles as $bundle) {
-            echo $bundle->getName() . PHP_EOL;
+            $bundleName = $bundle->getName();
+            echo $storage->getBundleVersion($bundleName) . ' # '
+                 . $bundleName . PHP_EOL;
         }
     }
 
@@ -33,6 +36,7 @@ class IndexController extends AbstractActionController
         $storageAdapter = $this->getInstallScriptStorage()->getAdapter();
 
         $changed = false;
+
         $bundles = $this->getInstallScriptLocator()->getBundles();
         foreach ($bundles as $bundle) {
             $currentVersion = $storageAdapter->getBundleVersion($bundle->getName());
@@ -40,8 +44,50 @@ class IndexController extends AbstractActionController
             $bundleName     = $bundle->getName();
 
             if ($maxVersion > $currentVersion) {
-                $storageAdapter->setBundleVersion($bundle->getName(), $maxVersion);
+                $storageAdapter->setBundleVersion($bundleName, $maxVersion);
                 echo $currentVersion . ' => ' . $maxVersion . ' # ' . $bundleName . PHP_EOL;
+                $changed = true;
+            } else {
+                echo $currentVersion . ' no changes # ' . $bundleName . PHP_EOL;
+            }
+        }
+
+        if ($changed) {
+            if ($storageAdapter->save()) {
+                echo 'Saved' .  PHP_EOL;
+            } else {
+                echo 'Failed to save data' .  PHP_EOL;
+            }
+        } else {
+            echo 'Nothing to save' .  PHP_EOL;
+        }
+    }
+
+    public function setAction()
+    {
+        $request = $this->getRequest();
+
+        $bundleName = $request->getParam('bundle');
+        $version  = $request->getParam('version');
+
+        $storageAdapter = $this->getInstallScriptStorage()->getAdapter();
+
+        $changed = false;
+
+        $bundles = $this->getInstallScriptLocator()->getBundles();
+        foreach ($bundles as $bundle) {
+            $currentBundleName = str_replace('\\', '', $bundle->getName());
+            $searchBundleName  = str_replace('\\', '', $bundleName);
+
+            if ($currentBundleName != $searchBundleName) {
+                continue;
+            }
+
+            $currentVersion = $storageAdapter->getBundleVersion($bundle->getName());
+
+            if ($version != $currentVersion) {
+                $storageAdapter->setBundleVersion($bundle->getName(), $version);
+                echo $currentVersion . ' => ' . $version . ' # ' . $bundleName . PHP_EOL;
                 $changed = true;
             } else {
                 echo $currentVersion . ' no changes # ' . $bundleName . PHP_EOL;
@@ -61,20 +107,13 @@ class IndexController extends AbstractActionController
 
     public function updateAction()
     {
+
+
         echo __METHOD__;
     }
 
     public function installAction()
     {
-        echo __METHOD__;
-    }
-
-    public function setAction()
-    {
-        $request = $this->getRequest();
-
-        $resource = $request->getParam('bundle');
-        $version  = $request->getParam('version');
 
 
         echo __METHOD__;
