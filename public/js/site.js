978b8b2a91d2769c7a42dbe565682bf45b54e4ae
Andrejs Stepanovs <a.stepanovs@mysportgroup.de>
2013-10-13 12:10:30 +0200

    update

--- public/js/site.js
+++ public/js/site.js
@@ -2,4 +2,10 @@ $(document).bind("mobileinit", function(){
     $.extend($.mobile, {
         ajaxEnabled: false
     });
+
+    function pieChartSliceSelected(primary, secondary)
+    {
+        console.log(primary);
+        console.log(secondary);
+    }
 });
