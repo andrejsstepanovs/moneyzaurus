6e01da2bc1c0ebe76c6cb39bcd460ea3573bc59c
Andrejs Stepanovs <a.stepanovs@mysportgroup.de>
2014-01-13 11:58:20 +0100

    index.php

--- php/index.php
+++ php/index.php
@@ -6,4 +6,14 @@ ini_set('display_errors', 1);
 
 //echo phpinfo();
 
-include 'public/index.php';
+//include 'public/index.php';
+
+
+
+//chdir(dirname(__DIR__));
+
+// Setup autoloading
+require 'init_autoloader.php';
+
+// Run the application!
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
