aecb5df987895ce603d21a97411a58c14d3e5fcc
Andrejs Stepanovs <a.stepanovs@mysportgroup.de>
2014-01-13 21:17:19 +0100

    delete gitignore

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
-chdir(dirname(__DIR__));
+
+chdir(__DIR__);
 
 // Setup autoloading
 require 'init_autoloader.php';
