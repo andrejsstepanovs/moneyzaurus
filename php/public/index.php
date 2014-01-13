dc675ce3e29014fa2ca175a3799ed754efdadc73
Andrejs Stepanovs <a.stepanovs@mysportgroup.de>
2014-01-13 12:02:38 +0100

    index.php

--- php/public/index.php
+++ php/public/index.php
@@ -5,8 +5,12 @@
  */
 chdir(dirname(__DIR__));
 
+echo __DIR__;
+echo "<br />";
+
 // Setup autoloading
-require 'init_autoloader.php';
+//require 'init_autoloader.php';
+include 'vendor/autoload.php';
 
 // Run the application!
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
