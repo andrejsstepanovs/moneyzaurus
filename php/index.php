3cfd836171a2a11f000bdceb20a47441d080a741
Andrejs Stepanovs <a.stepanovs@mysportgroup.de>
2014-01-13 11:38:37 +0100

    set main dir

--- php/index.php
+++ php/index.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+chdir(dirname(__DIR__));
+
 //echo phpinfo();
 
 include 'public/index.php';
--- php/public/index.php
+++ php/public/index.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
-chdir(dirname(__DIR__));
 
 // Setup autoloading
 require 'init_autoloader.php';
