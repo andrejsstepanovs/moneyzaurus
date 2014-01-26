584478402ad202fb9cc72bee6de958fe9a29fd8e
Andrejs Stepanovs <a.stepanovs@mysportgroup.de>
2014-01-26 19:05:37 +0100

    email html

--- php/module/Application/src/Application/Helper/ResendPassword/Helper.php
+++ php/module/Application/src/Application/Helper/ResendPassword/Helper.php
@@ -4,6 +4,8 @@ namespace Application\Helper\ResendPassword;
 
 use Application\Helper\AbstractHelper;
 use Zend\Mail\Message;
+use Zend\Mime\Part as MimePart;
+use Zend\Mime\Message as MimeMessage;
 
 /**
  * Class Helper
@@ -19,16 +21,22 @@ class Helper extends AbstractHelper
      */
     public function getMailMessage(\Db\ActiveRecord $user)
     {
-        $message = array();
-        $message[] ='Hi!';
-        $message[] ='New password is ' . $user->getPassword() . '';
+        $mimeParts = array();
+        $message = $this->getBodyData($user);
+
+        $html = new MimePart(implode('<br />', $message));
+        $html->type = 'text/html';
+        $mimeParts[] = $html;
+
+        $body = new MimeMessage();
+        $body->setParts($mimeParts);
 
         $mail = new Message();
         $mail->addTo($user->getEmail())
              ->setEncoding('UTF-8')
-             ->setSubject('moneyzaurus.com email reset')
+             ->setSubject('New moneyzaurus.com password')
              ->setFrom('moneyzaurusapp@gmail.com')
-             ->setBody(implode('\r\n', $message));
+             ->setBody($body);
 
         return $mail;
     }
@@ -39,5 +47,31 @@ class Helper extends AbstractHelper
     public function getNewPassword()
     {
         return uniqid();
+    }
+
+    /**
+     * @param \Db\ActiveRecord $user
+     *
+     * @return array
+     */
+    protected function getBodyData(\Db\ActiveRecord $user)
+    {
+        $message = array();
+        $message[] = '<html>';
+        $message[] = '<body>';
+        $message[] = '<h3>Hi ' . $user->getEmail() . '!</h3>';
+        $message[] = '';
+        $message[] = 'You asked for new password?';
+        $message[] = '';
+        $message[] = 'Here it is: <strong>' . $user->getPassword() . '</strong>';
+        $message[] = '';
+        $message[] = '';
+        $message[] = 'Have a nice day!';
+        $message[] = '';
+        $message[] = '<a href="http://www.moneyzaurus.com" target="_blank">moneyzaurus.com</a>';
+        $message[] = '</body>';
+        $message[] = '</html>';
+
+        return $message;
     }
 }
