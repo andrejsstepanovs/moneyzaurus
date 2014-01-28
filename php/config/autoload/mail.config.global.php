31b8d89dc247ad9fd2443164b6d153c86013f68a
Andrejs Stepanovs <a.stepanovs@mysportgroup.de>
2014-01-28 10:21:01 +0100

    using env variables to setup email

--- php/config/autoload/mail.config.global.php
+++ php/config/autoload/mail.config.global.php
@@ -1,16 +1,30 @@
 <?php
+$host = getenv('APP_MAIL_HOST');
+$host = empty($host) ? 'smtp.gmail.com' : $host;
+
+$username = getenv('APP_MAIL_USERNAME');
+$username = empty($username) ? '*****@gmail.com' : $username;
+
+$password = getenv('APP_MAIL_PASSWORD');
+$password = empty($password) ? '*******' : $password;
+
+$port = getenv('APP_MAIL_PORT');
+$port = empty($port) ? '587' : $port;
+
+$ssl = getenv('APP_MAIL_TLS');
+$ssl = empty($ssl) ? 'tls' : $ssl;
 
 return array(
     'mail' => array(
         'transport' => array(
             'options' => array(
-                'host'              => 'smtp.gmail.com',
+                'host'              => $host,
                 'connection_class'  => 'plain',
                 'connection_config' => array(
-                    'username'      => '******@gmail.com',
-                    'port'          => '587',
-                    'password'      => '******',
-                    'ssl'           => 'tls'
+                    'username'      => $username,
+                    'port'          => $port,
+                    'password'      => $password,
+                    'ssl'           => $ssl
                 )
             )
         )
