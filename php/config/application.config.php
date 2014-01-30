8a279a6495c2da65c7e42179b07f39de015be68f
Andrejs Stepanovs <andrejsstepanovs@gmail.com>
2014-01-30 20:43:46 +0100

    enabled config and module cache

--- php/config/application.config.php
+++ php/config/application.config.php
@@ -17,30 +17,11 @@ return array(
         'config_glob_paths' => array(
             'config/autoload/{,*.}{global,local}.php',
         ),
-
-        // Whether or not to enable a configuration cache.
-        // If enabled, the merged configuration will be cached and used in
-        // subsequent requests.
-        //'config_cache_enabled' => $booleanValue,
-
-        // The key used to create the configuration cache file name.
-        //'config_cache_key' => $stringKey,
-
-        // Whether or not to enable a module class map cache.
-        // If enabled, creates a module class map cache which will be used
-        // by in future requests, to reduce the autoloading process.
-        //'module_map_cache_enabled' => $booleanValue,
-
-        // The key used to create the class map cache file name.
-        //'module_map_cache_key' => $stringKey,
-
-        // The path in which to cache merged configuration.
-        //'cache_dir' => $stringPath,
-
-        // Whether or not to enable modules dependency checking.
-        // Enabled by default, prevents usage of modules that depend on other modules
-        // that weren't loaded.
-        // 'check_dependencies' => true,
+        'config_cache_enabled'     => true,
+        'config_cache_key'         => 's4DCtFGs',
+        'module_map_cache_enabled' => false,
+        'module_map_cache_key'     => 'EDPnM9Rk',
+        'cache_dir'                => './data/cache/config',
     ),
 
     // Used to create an own service manager. May contain one or more child arrays.
